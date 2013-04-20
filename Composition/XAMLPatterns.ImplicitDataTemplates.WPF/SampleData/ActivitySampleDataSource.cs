f722c90be7bfd74aa04fa102bb035f640e6b4a11
Michael L Perry <michael@qedcode.com>
2013-04-20 16:11:48 -0500

    Implicit Data Templates

--- Composition/XAMLPatterns.ImplicitDataTemplates.WPF/SampleData/ActivitySampleDataSource.cs
+++ Composition/XAMLPatterns.ImplicitDataTemplates.WPF/SampleData/ActivitySampleDataSource.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using XAMLPatterns.ImplicitDataTemplates.WPF.Models;
 
 namespace XAMLPatterns.ImplicitDataTemplates.WPF.SampleData
