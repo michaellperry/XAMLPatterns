deb7eae5b506f3c14615fbf12ef913756335ee90
Michael L Perry <michael@qedcode.com>
2013-04-20 13:52:30 -0500

    WinRT example of Implicit Data Templates.

--- Composition/XAMLPatterns.ImplicitDataTemplates.WinRT/SampleData/ActivitySampleDataSource.cs
+++ Composition/XAMLPatterns.ImplicitDataTemplates.WinRT/SampleData/ActivitySampleDataSource.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using XAMLPatterns.ImplicitDataTemplates.WinRT.Models;
 
 namespace XAMLPatterns.ImplicitDataTemplates.WinRT.SampleData
---
+++ Composition/XAMLPatterns.ImplicitDataTemplates.WinRT/ValueConverters/DateFormatValueConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace XAMLPatterns.ImplicitDataTemplates.WinRT.ValueConverters
+{
+    public class DateFormatValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return string.Format("{0:M}", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
