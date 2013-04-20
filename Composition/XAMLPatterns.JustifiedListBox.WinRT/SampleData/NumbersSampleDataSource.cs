eeae3d48212665e60aa00aef60f5dbff44c34c58
Michael L Perry <michael@qedcode.com>
2013-04-20 16:46:00 -0500

    Justified List Box

---
+++ Composition/XAMLPatterns.JustifiedListBox.WinRT/SampleData/NumbersSampleDataSource.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace XAMLPatterns.JustifiedListBox.WPF.SampleData
+{
+    public class NumbersSampleDataSource
+    {
+        private IEnumerable<string> _numbers = new List<string>
+        {
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six"
+        };
+
+        public IEnumerable<string> Numbers
+        {
+            get { return _numbers; }
+        }
+    }
+}
