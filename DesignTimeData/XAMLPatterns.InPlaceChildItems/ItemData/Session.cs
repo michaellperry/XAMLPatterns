daa5da19b402ceb739f236bd6504ca343e130767
Michael L Perry <michael@qedcode.com>
2013-05-11 17:33:52 -0500

    In place data and in place child items.

---
+++ DesignTimeData/XAMLPatterns.InPlaceChildItems/ItemData/Session.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace XAMLPatterns.InPlaceChildItems.ItemData
+{
+    public class Session
+    {
+        public string Title { get; set; }
+        public string Speaker { get; set; }
+    }
+}
