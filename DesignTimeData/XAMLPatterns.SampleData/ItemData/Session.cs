6b8ff63f4a3aab45d9dc7abd544acc82ae8ba7b2
Michael L Perry <michael@qedcode.com>
2013-05-11 18:07:55 -0500

    Sample data.

---
+++ DesignTimeData/XAMLPatterns.SampleData/ItemData/Session.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace XAMLPatterns.SampleData.ItemData
+{
+    public class Session
+    {
+        public string Title { get; set; }
+        public string Speaker { get; set; }
+    }
+}
