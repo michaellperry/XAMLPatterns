d50cd95ced6c2f818d1016a2d28e3b370b20f425
Michael L Perry <michael@qedcode.com>
2013-05-24 20:47:50 -0500

    Visual State Binding.

---
+++ Animation/XAMLPatterns.VisualStateBinding/ViewModels/CompassPoint.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace XAMLPatterns.VisualStateBinding.ViewModels
+{
+    public enum CompassPoint
+    {
+        North,
+        South,
+        East,
+        West
+    }
+}
---
+++ Animation/XAMLPatterns.VisualStateBinding/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,12 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace XAMLPatterns.VisualStateBinding.ViewModels
+{
+    public class ViewModelLocator : ViewModelBase
+    {
+        public MainViewModel Main
+        {
+            get { return new MainViewModel(); }
+        }
+    }
+}
